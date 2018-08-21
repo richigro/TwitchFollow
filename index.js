326e7e6879d22d1671ea5ad34669bf9b61f635ae
Ricardo Guerrero <richi_gro@hotmail.com>
2018-08-21 17:06:14 -0500

    testing twicth api

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("connected to index.js");
+const url = "endpoint here"
+
+function getDataFromApi(){
+    $.ajax({
+        url: url,
+        success: function(data) {
+            console.log(data);
+        }
+    });
+}
+
+
+function appInstance() {
+    getDataFromApi();
+}
+
+
+$(appInstance);
