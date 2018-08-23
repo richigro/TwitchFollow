655579195caf87e78571dea911e9a9b6e9d3f198
Ricardo Guerrero <richi_gro@hotmail.com>
2018-08-22 20:50:04 -0500

    etsy test

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-const url = "https://openapi.etsy.com/v2/listings/active?api_key=wx9upjki3704ec3rnuf1r87c"
-
+const url = "https://openapi.etsy.com/v2/listings/active?limit=50&offset=0&api_key=wx9upjki3704ec3rnuf1r87c"
 function getDataFromApi(){
     $.ajax({
         url: url,
